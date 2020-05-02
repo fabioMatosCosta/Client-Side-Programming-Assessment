4095a99e7c1f4d39c25f1475e64a4fc8f1e9e608
Fábio Costa <fabiodanielmc@gmail.com>
2020-05-02 14:07:50 +0200

    first commit

---
+++ api-calls/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors({
+    origin: ['https://localhost:3000', 'http://localhost:3000'],
+    credentials: true
+}));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api_calls');
+
+app.use('/', indexRouter);
+app.use('/beers', apiRouter);
+
+module.exports = app;
---
+++ api-calls/routes/api_calls.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const axios = require("axios");
+
+const apiKey = process.env.KEY
+const apiURL = process.env.URL
+
+/* GET beers from api. */
+router.get('/', function(req, res, next) {
+  const url = `http://api.brewerydb.com/v2/beers/?key=659d5c6b8f3d2447f090119e48202fdb`;
+  axios
+    .get(url)
+    .then(response =>{
+      res.json({beers: response.data})
+    })
+    .catch(err => {
+      console.log(`get beers error: ${err}`)
+      res.status(500);
+      res.json({err})
+    })
+});
+
+module.exports = router;
